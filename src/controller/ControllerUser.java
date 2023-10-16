b6f805f369d859cb42abc544234d1b7567991deb
Emilie <snapmili@live.fr>
2023-10-16 15:21:40 +0200

    add controller

---
+++ src/controller/ControllerUser.java
@@ -0,0 +1,15 @@
+package controller;
+import model.UserType;
+public class ControllerUser {
+
+    public ControllerUser() {
+    }
+
+
+    public void addUser(UserType type, String name, String firstname, String mail, String password){
+
+    }
+    public void connectUser(UserType type, String mail, String password){
+
+    }
+}
---
+++ src/model/UserType.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum UserType {
+    Vulnerable,
+    Validator,
+    Volunteer
+
+}
