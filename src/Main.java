e1155702058c0f0a2692d11738c887d33cf39061
st4rhub <oceanepioche@gmail.com>
2023-10-20 18:20:32 +0200

    fenêtre login -> changement main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import vue.GUI;
 
 public class Main {
     public static void main(String[] args){
-        GUI page = new GUI();
+        //GUI page = new GUI();
+        GUI.Registration page2 = new GUI.Registration();
     }
 }
--- src/vue/GUI.java
+++ src/vue/GUI.java
@@ -6,7 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI {
+
     public GUI(){
+        homeScreen();
+    }
+    private void homeScreen(){
         try {UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (Exception e) { e.printStackTrace(); }
 
@@ -41,12 +45,6 @@ public class GUI {
 
         pane.add(p, BorderLayout.PAGE_END);
 
-        vuln.addActionListener(new ActionListener() {
-             public void actionPerformed(ActionEvent e) {
-                //this.moveFrame();
-             }
-        });
-
         // make window's dimension fit its content
         f.pack();
         f.setVisible(true);
@@ -61,6 +59,106 @@ public class GUI {
 
     public static class Registration{
 
+        public Registration(){
+            signup();
+        }
+
+        private void signup(){
+
+            //Set up window
+            JFrame f = new JFrame("Connexion");
+            f.setResizable(false);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setBounds(300, 90, 900, 600);
+
+            Container c = f.getContentPane();
+            c.setLayout(new FlowLayout());
+
+            JLabel title = new JLabel("Connexion");
+            title.setFont(new Font("Arial", Font.PLAIN, 30));
+            title.setSize(350, 30);
+            title.setHorizontalAlignment(SwingConstants.CENTER);
+            c.add(title);
+
+            //PARAMETRES DE CONNEXION
+
+            //Nom
+            JLabel surname = new JLabel("Nom :");
+            surname.setFont(new Font("Arial", Font.PLAIN, 20));
+            surname.setSize(100,20);
+            surname.setLocation(100, 100);
+            c.add(surname);
+
+            JTextField tsurname = new JTextField();
+            tsurname.setFont(new Font("Arial", Font.PLAIN, 15));
+            tsurname.setSize(190, 20);
+            tsurname.setLocation(200, 100);
+            c.add(tsurname);
+
+            //Prénom
+            JLabel name = new JLabel("Prénom :");
+            name.setFont(new Font("Arial", Font.PLAIN, 20));
+            name.setSize(100, 20);
+            name.setLocation(100, 150);
+            c.add(name);
+
+            JTextField tname = new JTextField();
+            tname.setFont(new Font("Arial", Font.PLAIN, 15));
+            tname.setSize(190, 20);
+            tname.setLocation(200, 150);
+            c.add(tname);
+
+            //E-mail
+            JLabel email = new JLabel("Adresse mail:");
+            email.setFont(new Font("Arial", Font.PLAIN, 20));
+            email.setSize(100, 200);
+            email.setLocation(100,200);
+            c.add(email);
+
+            JTextField temail = new JTextField();
+            temail.setFont(new Font("Arial", Font.PLAIN, 15));
+            temail.setSize(190, 20);
+            temail.setLocation(200, 200);
+            c.add(temail);
+
+            //Password
+            JLabel password = new JLabel("Mot de passe :");
+            password.setFont(new Font("Arial", Font.PLAIN, 20));
+            password.setSize(100, 20);
+            password.setLocation(100, 250);
+            c.add(password);
+
+            JPasswordField tpassword = new JPasswordField();
+            tpassword.setFont(new Font("Arial", Font.PLAIN, 15));
+            tpassword.setSize(190, 20);
+            tpassword.setLocation(200, 250);
+            c.add(tpassword);
+
+            //Bouton connexion
+            JButton button = new JButton("Connexion"); //set label to button
+
+        /*
+            //create panel to put form elements
+            JPanel p = new JPanel(new GridLayout(3, 1));
+            p.add(surname);   //Set surname label to panel
+            p.add(tsurname);   //Set surname field to panel
+            p.add(name);    //set username label to panel
+            p.add(tname);   //set text field to panel
+            p.add(email);   //Set email label to panel
+            p.add(temail);   //Set email field to panel
+            p.add(password);    //set password label to panel
+            p.add(tpassword);   //set text field to panel
+            p.add(button);           //set button to panel
+
+
+            //set border to panel
+            c.add(p, BorderLayout.CENTER);
+            //Display the panel
+            p.setVisible(true);
+        */
+            //Frame config
+            f.setVisible(true);
+        }
     }
 }
 
