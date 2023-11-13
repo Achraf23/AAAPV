8323a6d959ce4390f7bd3a4501c715cb7bb8a9d2
st4rhub <oceanepioche@gmail.com>
2023-11-13 10:26:36 +0100

    GUI page connexion layout

--- src/vue/GUI.java
+++ src/vue/GUI.java
@@ -26,42 +26,40 @@ public class GUI {
 
         Container pane = f.getContentPane();
 
-        //label with text area
+        //Label with text area
         final JLabel text = new JLabel("Quel type d'utilisateur.trice êtes-vous ?");
         text.setHorizontalAlignment(SwingConstants.CENTER);
         pane.add(text, BorderLayout.CENTER);
 
         //Create the panel and the 3 buttons inside
-        JPanel p = new JPanel(new BorderLayout());
+        JPanel p = new JPanel();
         p.setLayout(new FlowLayout());
 
-        //maybe frame sur mode vulerable par defaut et opt benev ou valideur en bas (??)
-        JButton vuln = new JButton("Demandeur d'aide");
-        //vuln.setBounds(100, 100, 150, 50);
+        JButton vuln = new JButton("Demandeur.euse d'aide");
         p.add(vuln);
         ControllerVulnerable controllerVulnerable = new ControllerVulnerable();
         controllerVulnerable.addListener(vuln);
 
         JButton benev = new JButton("Bénévole");
-        //benev.setBounds(100, 150, 100, 50);
         p.add(benev);
         ControllerVolunteer controllerVolunteer = new ControllerVolunteer();
         controllerVulnerable.addListener(benev);
 
-        JButton valid = new JButton("Valideur");
-        //valid.setBounds(100, 200, 100, 50);
+        JButton valid = new JButton("Valideur.euse");
         p.add(valid);
         ControllerValidator controllerValidator = new ControllerValidator();
         controllerVulnerable.addListener(valid);
 
         pane.add(p, BorderLayout.PAGE_END);
 
-        // make window's dimension fit its content
+        //Make window's dimension fit its content
         f.pack();
         f.setVisible(true);
         text.setVisible(true);
+
         //Display the panel
         p.setVisible(true);
+
         //Display the buttons
         vuln.setVisible(true);
         benev.setVisible(true);
@@ -79,16 +77,15 @@ public class GUI {
             //Set up window
             JFrame f = new JFrame("Connexion");
             f.setResizable(false);
-            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.setBounds(300, 90, 900, 600);
+            f.setBounds(300, 90, 600, 450);
 
             Container c = f.getContentPane();
-            c.setLayout(new FlowLayout());
+            c.setLayout(null);
 
             JLabel title = new JLabel("Connexion");
             title.setFont(new Font("Arial", Font.PLAIN, 30));
             title.setSize(350, 30);
-            title.setHorizontalAlignment(SwingConstants.CENTER);
+            title.setLocation(210, 25);
             c.add(title);
 
             //PARAMETRES DE CONNEXION
@@ -102,8 +99,8 @@ public class GUI {
 
             JTextField tsurname = new JTextField();
             tsurname.setFont(new Font("Arial", Font.PLAIN, 15));
-            tsurname.setSize(190, 20);
-            tsurname.setLocation(200, 100);
+            tsurname.setSize(190, 30);
+            tsurname.setLocation(300, 100);
             c.add(tsurname);
 
             //Prénom
@@ -115,60 +112,48 @@ public class GUI {
 
             JTextField tname = new JTextField();
             tname.setFont(new Font("Arial", Font.PLAIN, 15));
-            tname.setSize(190, 20);
-            tname.setLocation(200, 150);
+            tname.setSize(190, 30);
+            tname.setLocation(300, 150);
             c.add(tname);
 
             //E-mail
             JLabel email = new JLabel("Adresse mail:");
             email.setFont(new Font("Arial", Font.PLAIN, 20));
-            email.setSize(100, 200);
+            email.setSize(200, 20);
             email.setLocation(100,200);
             c.add(email);
 
             JTextField temail = new JTextField();
             temail.setFont(new Font("Arial", Font.PLAIN, 15));
-            temail.setSize(190, 20);
-            temail.setLocation(200, 200);
+            temail.setSize(190, 30);
+            temail.setLocation(300, 200);
             c.add(temail);
 
             //Password
             JLabel password = new JLabel("Mot de passe :");
             password.setFont(new Font("Arial", Font.PLAIN, 20));
-            password.setSize(100, 20);
+            password.setSize(200, 20);
             password.setLocation(100, 250);
             c.add(password);
 
             JPasswordField tpassword = new JPasswordField();
             tpassword.setFont(new Font("Arial", Font.PLAIN, 15));
-            tpassword.setSize(190, 20);
-            tpassword.setLocation(200, 250);
+            tpassword.setSize(190, 30);
+            tpassword.setLocation(300, 250);
             c.add(tpassword);
 
             //Bouton connexion
-            JButton button = new JButton("Connexion"); //set label to button
+            JButton connect = new JButton("Connexion"); //set label to button
+            connect.setFont(new Font("Arial", Font.PLAIN, 15));
+            connect.setSize(200, 40);
+            connect.setLocation(200, 325);
+            c.add(connect);
 
-        /*
-            //create panel to put form elements
-            JPanel p = new JPanel(new GridLayout(3, 1));
-            p.add(surname);   //Set surname label to panel
-            p.add(tsurname);   //Set surname field to panel
-            p.add(name);    //set username label to panel
-            p.add(tname);   //set text field to panel
-            p.add(email);   //Set email label to panel
-            p.add(temail);   //Set email field to panel
-            p.add(password);    //set password label to panel
-            p.add(tpassword);   //set text field to panel
-            p.add(button);           //set button to panel
-
-
-            //set border to panel
-            c.add(p, BorderLayout.CENTER);
-            //Display the panel
-            p.setVisible(true);
-        */
-            //Frame config
+            //Make window visible
             f.setVisible(true);
+
+            //Display the button
+            connect.setVisible(true);
         }
     }
 }
