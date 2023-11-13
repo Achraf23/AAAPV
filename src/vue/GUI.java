9d67f75de385935094aeba7c60c0db407756ca32
st4rhub <oceanepioche@gmail.com>
2023-11-13 10:37:32 +0100

    GUI update

--- src/vue/GUI.java
+++ src/vue/GUI.java
@@ -39,15 +39,12 @@ public class GUI {
         JPanel p = new JPanel();
         p.setLayout(new FlowLayout());
 
-        //maybe frame sur mode vulerable par defaut et opt benev ou valideur en bas (??)
         JButton vuln = new JButton("Demandeur d'aide");
-        //vuln.setBounds(100, 100, 150, 50);
         p.add(vuln);
         this.controllerUser = new ControllerVulnerable(this);
         controllerUser.addListener(vuln);
 
         JButton benev = new JButton("Bénévole");
-        //benev.setBounds(100, 150, 100, 50);
         p.add(benev);
         this.controllerUser = new ControllerVolunteer(this);
         controllerUser.addListener(benev);
@@ -117,12 +114,12 @@ public class GUI {
             tname.setLocation(300, 150);
             c.add(tname);
 
-            //E-mail
-            JLabel email = new JLabel("Adresse mail:");
-            email.setFont(new Font("Arial", Font.PLAIN, 20));
-            email.setSize(200, 20);
-            email.setLocation(100,200);
-            c.add(email);
+        //E-mail
+        JLabel email = new JLabel("Adresse mail:");
+        email.setFont(new Font("Arial", Font.PLAIN, 20));
+        email.setSize(200, 20);
+        email.setLocation(100,200);
+        c.add(email);
 
             JTextField temail = new JTextField();
             temail.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -130,12 +127,12 @@ public class GUI {
             temail.setLocation(300, 200);
             c.add(temail);
 
-            //Password
-            JLabel password = new JLabel("Mot de passe :");
-            password.setFont(new Font("Arial", Font.PLAIN, 20));
-            password.setSize(200, 20);
-            password.setLocation(100, 250);
-            c.add(password);
+        //Password
+        JLabel password = new JLabel("Mot de passe :");
+        password.setFont(new Font("Arial", Font.PLAIN, 20));
+        password.setSize(200, 20);
+        password.setLocation(100, 250);
+        c.add(password);
 
             JPasswordField tpassword = new JPasswordField();
             tpassword.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -145,39 +142,9 @@ public class GUI {
 
         //Bouton connexion
         JButton button = new JButton("Connexion"); //set label to button
+        c.add(button);
         controllerUser.test();
 
-    /*
-        //create panel to put form elements
-        JPanel p = new JPanel(new GridLayout(3, 1));
-        p.add(surname);   //Set surname label to panel
-        p.add(tsurname);   //Set surname field to panel
-        p.add(name);    //set username label to panel
-        p.add(tname);   //set text field to panel
-        p.add(email);   //Set email label to panel
-        p.add(temail);   //Set email field to panel
-        p.add(password);    //set password label to panel
-        p.add(tpassword);   //set text field to panel
-        p.add(button);           //set button to panel
-            //Bouton connexion
-            JButton connect = new JButton("Connexion"); //set label to button
-            connect.setFont(new Font("Arial", Font.PLAIN, 15));
-            connect.setSize(200, 40);
-            connect.setLocation(200, 325);
-            c.add(connect);
-
-            //Make window visible
-            f.setVisible(true);
-
-            //Display the button
-            connect.setVisible(true);
-        }
-
-        //set border to panel
-        c.add(p, BorderLayout.CENTER);
-        //Display the panel
-        p.setVisible(true);
-    */
         //Frame config
         f.setVisible(true);
     }
