b10cb0533a5cc5757a0ea58bb100c2799a1d7e97
bensebaa <achrafbensebaa23@gmail.com>
2023-12-08 11:13:47 +0100

    work on tests + getMissions dans controller pour Emilie

--- src/main/java/controller/MissionController.java
+++ src/main/java/controller/MissionController.java
@@ -2,6 +2,9 @@ package controller;
 import model.Mission;
 import model.Database;
 
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+
 public class MissionController {
     Database db;
 
@@ -9,9 +12,12 @@ public class MissionController {
         Database.connectToDatabase();
     }
 
-    void insertMissionIntoDatabase(Mission m){
+    public void insertMissionIntoDatabase(Mission m){
         Database.insertLineIntoMission(m);
+    }
 
+    public ArrayList<Mission> getMissions(){
+        return Database.getAllMissions();
     }
 
 }
